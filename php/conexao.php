75bd5272a1257dbe6755f56da0f0e646c8ec166f
Laiane Ricardo <ricardolaiane@gmail.com>
2022-07-10 16:05:29 -0300

    primeiro commit

---
+++ php/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+//Variáveis de acesso
+$url = "localhost";
+$usuario = "root";
+$senha = "";
+$base= "api";
+
+//conexão
+$conexao = mysqli_connect($url, $usuario, $senha, $base);
+
+//Arrumar caracteres especiais
+mysqli_set_charset($conexao, "utf8");
+?>
+    
